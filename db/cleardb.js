2bf0a8a42258a6be9726b934fa8463973a2daef3
Cameron Barr <cjbarr1013@gmail.com>
2025-05-30 16:38:35 -0400

    db testing...

--- db/cleardb.js
+++ db/cleardb.js
@@ -3,21 +3,12 @@ const { Client } = require('pg');
 const { argv } = require('node:process');
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS messages (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  text VARCHAR ( 255 ),
-  username VARCHAR ( 255 ),
-  added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO messages (text, username)
-VALUES
-  ('Hi there!', 'Amando'),
-  ('Hello World!', 'Charles'); 
+DELETE FROM messages; 
 `;
 
 async function main() {
   console.log('seeding...');
+  console.log('connection string:', argv[2]);
   const client = new Client({
     connectionString: argv[2],
   });
